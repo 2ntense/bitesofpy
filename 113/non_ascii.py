ce36d0b0cb6dc068bb92df316a1a6f07120b2ce2
2ntense <stefanyip@hotmail.com>
2019-11-04 21:44:46 +0100

    bite 113 better solution

--- 113/non_ascii.py
+++ 113/non_ascii.py
@@ -3,4 +3,4 @@ import string
 
 def extract_non_ascii_words(text):
     """Filter a text returning a list of non-ascii words"""
-    return [w for w in text.split() if not all(c in string.ascii_letters or c in string.punctuation or c in string.digits for c in w)]
+    return [w for w in text.split() if not all(ord(c) < 128 for c in w)]
