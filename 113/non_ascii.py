d6e5f8ddf9faf09f351682c15e5dfc0c4c801f83
2ntense <stefanyip@hotmail.com>
2019-11-04 21:45:07 +0100

    bite 113 remove redundant import

--- 113/non_ascii.py
+++ 113/non_ascii.py
@@ -1,6 +1,3 @@
-import string
-
-
 def extract_non_ascii_words(text):
     """Filter a text returning a list of non-ascii words"""
     return [w for w in text.split() if not all(ord(c) < 128 for c in w)]
