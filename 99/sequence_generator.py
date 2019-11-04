c4d7d148a816e31daa93131d6bc42515423a7987
2ntense <stefanyip@hotmail.com>
2019-11-04 21:16:29 +0100

    bite 99 attempt 1

---
+++ 99/sequence_generator.py
@@ -0,0 +1,10 @@
+import itertools
+
+
+def sequence_generator():
+    def sequence():
+        for i in range(1, 26 + 1):
+            yield i
+            yield chr(i + 64)
+
+    itertools.cycle(sequence())
