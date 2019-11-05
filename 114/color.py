75a4dcf038b45e6772e7ff5cc766af462d59daaf
2ntense <stefanyip@hotmail.com>
2019-11-05 02:12:37 +0100

    bite 114 attempt 2

--- 114/color.py
+++ 114/color.py
@@ -5,6 +5,7 @@ The following sites were consulted:
     https://www.webucator.com/blog/2015/03/python-color-constants-module/
 """
 import os
+import string
 import sys
 import urllib.request
 
@@ -25,32 +26,40 @@ class Color:
     """
 
     def __init__(self, color):
-        self.rgb = color
-        pass
+        try:
+            self.rgb = COLOR_NAMES[color.upper()]
+        except KeyError:
+            self.rgb = None
 
     @classmethod
     def hex2rgb(cls, hex):
         """Class method that converts a hex value into an rgb one"""
-        if not isinstance(hex, str):
-            raise TypeError
-        if len(hex) != 7:
+        if not isinstance(hex, str) or len(hex) != 7:
             raise ValueError
+
+        if not hex.startswith("#"):
+            raise ValueError
+
+        for c in hex[1:]:
+            if c not in string.hexdigits:
+                raise ValueError
 
         def spl(s: str) -> list:
             return [s[i] + s[i + 1] for i in range(0, len(s), 2)]
 
-        splitted = spl(hex[1:])
+        s = spl(hex[1:])
 
-        return int(splitted [0], 16), int(splitted [1], 16), int(splitted [2], 16)
-
+        return int(s[0], 16), int(s[1], 16), int(s[2], 16)
 
     @classmethod
     def rgb2hex(cls, rgb):
         """Class method that converts an rgb value into a hex one"""
-        if not isinstance(rgb, tuple):
-            raise TypeError
-        if len(rgb) != 3:
+        if not isinstance(rgb, tuple) or len(rgb) != 3:
             raise ValueError
+
+        for c in rgb:
+            if not isinstance(c, int) or not 0 <= c <= 255:
+                raise ValueError
 
         return "#" + "".join([hex(c)[2:] if len(hex(c)[2:]) == 2 else "0" + hex(c)[2:] for c in rgb])
 
@@ -60,4 +69,7 @@ class Color:
 
     def __str__(self):
         """Returns the string value of the color object"""
-        return f"{list(self.rgb)[0]}"
+        for k, v in COLOR_NAMES.items():
+            if v == self.rgb:
+                return k
+        return "Unknown"
