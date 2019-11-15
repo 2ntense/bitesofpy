933bebb4fd199d52a7f81d80299f05207167536f
2ntense <stefanyip@hotmail.com>
2019-11-15 20:24:57 +0100

    bite 141 support changes in dateformat enum

--- 141/primitive_date_inferrer.py
+++ 141/primitive_date_inferrer.py
@@ -67,14 +67,9 @@ def get_dates(dates):
     for d in dates:
         mdf = _maybe_DateFormats(d)
         for df in mdf:
-            if df == DateFormat.DDMMYY:
-                c[DateFormat.DDMMYY] += 1
-            elif df == DateFormat.MMDDYY:
-                c[DateFormat.MMDDYY] += 1
-            elif df == DateFormat.YYMMDD:
-                c[DateFormat.YYMMDD] += 1
-            else:
-                c[DateFormat.NONPARSABLE] += 1
+            for df_enum in DateFormat:
+                if df == df_enum:
+                    c[df_enum] += 1
 
     mc = c.most_common(2)
     if mc[0][1] == mc[1][1]:
@@ -84,7 +79,7 @@ def get_dates(dates):
     if most_prevalent == DateFormat.NONPARSABLE:
         raise InfDateFmtError()
 
-    df = DateFormat.get_d_parse_formats(val=most_prevalent.value)
+    df = DateFormat.get_d_parse_formats(most_prevalent.value)
 
     date_list = list()
     for d in dates:
@@ -94,21 +89,3 @@ def get_dates(dates):
             date_list.append("Invalid")
 
     return date_list
-
-
-dates = [
-    "04/25/79",
-    "08/09/70",
-    "08/04/10",
-    "95/31/10",
-    "06/13/34",
-    "04/03/22",
-    "67/12/17",
-    "34/10/12",
-    "04/05/94",
-    "07/12/41",
-    "88/11/05",
-    "96/26/08",
-]
-
-print(get_dates(dates))
