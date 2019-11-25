6055c0198bb941bffba7855f802525bee9501068
2ntense <stefanyip@hotmail.com>
2019-11-25 19:40:04 +0100

    bite 222

---
+++ 222/grouping.py
@@ -0,0 +1,26 @@
+def group(iterable, n):
+    """Splits an iterable set into groups of size n and a group
+       of the remaining elements if needed.
+
+       Args:
+         iterable (list): The list whose elements are to be split into
+                          groups of size n.
+         n (int): The number of elements per group.
+
+       Returns:
+         list: The list of groups of size n,
+               where each group is a list of n elements.
+    """
+    result = []
+    it = list(iterable)
+    while it:
+        result.append(it[:n])
+        it = it[n:]
+    return result
+
+
+if __name__ == '__main__':
+    iterable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    n = 3
+    ret = group(iterable, n)
+    print(ret)
