998b7d22e4f57a6bb3370ce659e3619f1073f1af
Stefan Yip <stefanyip@hotmail.com>
2019-11-26 02:07:16 +0100

    bite 233

--- 233/files.py
+++ 233/files.py
@@ -9,4 +9,14 @@ ZIP_FILE = 'logs.zip'
 
 def zip_last_n_files(directory: PosixPath = LOG_DIR,
                      zip_file: str = ZIP_FILE, n: int = 3):
-    pass
+    paths_dt = []
+    for log_file in directory.glob('*.log'):
+        creation_ts = log_file.stat().st_ctime
+        paths_dt.append((log_file, datetime.fromtimestamp(creation_ts)))
+    paths_dt.sort(key=lambda x: x[1], reverse=True)
+
+    with ZipFile(zip_file, 'w') as file:
+        for path, dt in paths_dt[:n]:
+            date_str = dt.strftime("%Y-%m-%d")
+            arcname = f"{path.stem}_{date_str}{path.suffix}"
+            file.write(path, arcname=arcname)
