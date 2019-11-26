eeddc805163f00501af3a1b41d56f745eb758220
Stefan Yip <stefanyip@hotmail.com>
2019-11-26 18:38:30 +0100

    bite 23

---
+++ 23/similarity.py
@@ -0,0 +1,40 @@
+import itertools
+import os
+import re
+from difflib import SequenceMatcher
+from urllib.request import urlretrieve
+
+# prep
+TAG_HTML = re.compile(r'<category>([^<]+)</category>')
+TEMPFILE = os.path.join('/tmp', 'feed')
+MIN_TAG_LEN = 10
+IDENTICAL = 1.0
+SIMILAR = 0.95
+
+urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/tags.xml',
+    TEMPFILE
+)
+
+
+def _get_tags(tempfile=TEMPFILE):
+    """Helper to parse all tags from a static copy of PyBites' feed,
+       providing this here so you can focus on difflib"""
+    with open(tempfile) as f:
+        content = f.read().lower()
+    # take a small subset to keep it performant
+    tags = TAG_HTML.findall(content)
+    tags = [tag for tag in tags if len(tag) > MIN_TAG_LEN]
+    return set(tags)
+
+
+def get_similarities(tags=None):
+    """Should return a list of similar tag pairs (tuples)"""
+    tags = tags or _get_tags()
+    result = []
+    s = SequenceMatcher()
+    for comb in itertools.combinations(tags, 2):
+        s.set_seqs(*comb)
+        if s.ratio() > 0.95:
+            result.append(comb)
+    return result
