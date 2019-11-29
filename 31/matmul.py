973c1d6ce178a22196623e2240a1594d390570d8
2ntense <stefanyip@hotmail.com>
2019-11-29 17:24:05 +0100

    bite 31

---
+++ 31/matmul.py
@@ -0,0 +1,17 @@
+class Matrix(object):
+
+    def __init__(self, values):
+        self.values = values
+
+    def __repr__(self):
+        return f'<Matrix values="{self.values}">'
+
+    def __matmul__(self, other):
+        return Matrix([[sum(x * other.values[i][col] for i, x in enumerate(row)) for col in range(len(other.values[0]))] for
+                row in self.values])
+
+    def __rmatmul__(self, other):
+        return self.__matmul__(other)
+
+    def __imatmul__(self, other):
+        return self.__matmul__(other)
